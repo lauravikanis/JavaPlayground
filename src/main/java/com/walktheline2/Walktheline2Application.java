2f977b8c200f98305c78c1eaa8d6e398811c35af
Laura Vikanis <vikanis@sipgate.de>
2021-05-28 14:03:07 +0200

    first commit

---
+++ src/main/java/com/walktheline2/Walktheline2Application.java
@@ -0,0 +1,13 @@
+package com.walktheline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Walktheline2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Walktheline2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/walktheline2/Walktheline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walktheline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Walktheline2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
